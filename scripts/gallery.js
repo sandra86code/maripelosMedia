176e2a4ea963cf5376f912b50e4332ad00f90d1c
sandra <sandra.ruizjimenez86@gmail.com>
2022-12-02 22:14:23 +0100

    Galeria, indice y booking abriendo hamburguesa correctamente

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,3 +1,11 @@
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
 function onClick(element) {
     document.getElementById("img01").src = element.src;
     document.getElementById("modal01").style.display = "block";
